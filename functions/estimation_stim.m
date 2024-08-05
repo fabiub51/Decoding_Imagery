65a350ee84de5e6d466aade8b69c96221ea00ef6
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-08-05 15:45:38 +0200

    Add files via upload

---
+++ functions/estimation_stim.m
@@ -0,0 +1,9 @@
+function estimation_stim(output_dir)
+
+file_dir = strcat(output_dir, '\SPM.mat');
+
+matlabbatch{1}.spm.stats.fmri_est.spmmat = {file_dir};
+matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
+
+spm_jobman('run', matlabbatch);
